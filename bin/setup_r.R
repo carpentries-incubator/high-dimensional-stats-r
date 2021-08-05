2447869ad6e7b87f340bfc7940c1230451a84224
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-08-05 18:43:24 +0100

    biocver

--- bin/setup_r.R
+++ bin/setup_r.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 install.packages(c("BiocManager"))
-BiocManager::install(version = "4.1")
+BiocManager::install(version = "3.13")
 deps <- read.table(
     url("https://raw.githubusercontent.com/carpentries-incubator/high-dimensional-stats-r/master/dependencies.csv")
 )
