5d276ef88ecddce26cf9f9d37534e0f5ce5ba62b
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2021-10-12 16:40:40 +0100

    Download data

--- bin/setup_r.R
+++ bin/setup_r.R
@@ -8,3 +8,21 @@ deps <- read.table(
 )
 BiocManager::install(deps[[1]])
 BiocManager::install("Alanocallaghan/shinystats")
+
+dir.create("data", showWarnings = FALSE)
+data_files <- c(
+    "cancer_expression.rds",
+    "coefHorvath.rds",
+    "methylation.rds",
+    "scrnaseq.rds",
+    "small_methylation.rds"
+)
+for (file in data_files) {
+    download.file(
+        url = file.path(
+            "https://raw.githubusercontent.com/carpentries-incubator/high-dimensional-stats-r/gh-pages",
+            file
+        ),
+        destfile = file.path("data", file)
+    )
+}
