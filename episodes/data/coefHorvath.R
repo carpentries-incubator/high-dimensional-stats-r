9e7159fc8d6d0d3cc8dcaeba7585d1dcc9733f91
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-02-03 18:51:30 +0000

    Delete data

--- episodes/data/coefHorvath.R
+++
@@ -1,9 +0,0 @@
-download.files("https://github.com/isglobal-brge/methylclock/raw/5cb84aa29c6d9ec5b93bfefd54338ccc1486e879/data/coefHorvath.rda", destfile=here::here("episodes/data/coefHorvath.rda"))
-load(here::here("episodes/data/coefHorvath.rda"))
-coef <- coefHorvath
-
-methylation <- readRDS(here::here("episodes/data/methylation.rds"))
-coef <- coef[coef$CpGmarker %in% rownames(methylation), ]
-coef <- coef[order(coef$CoefficientTrainingShrunk, decreasing = TRUE), ]
-
-saveRDS(coef, here::here("episodes/data/coefHorvath.rds"))
--- episodes/data/prostate.R
+++
@@ -1,4 +0,0 @@
-library("here")
-x <- read.csv(here("episodes/data/prostate.csv"))
-x <- x[, -1]
-saveRDS(x, here("episodes/data/prostate.rds"))
